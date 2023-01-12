6c8143c9dfe1f8828c7320626feea2617a8e4088
FrancoMastro07 <francomastro07@gmail.com>
2023-01-11 22:32:37 -0300

    Added database connection

--- First_Project/Tkinter.py
+++ First_Project/Tkinter.py
@@ -1,4 +1,31 @@
 from tkinter import *
+from tkinter import messagebox
+import sqlite3
+
+#----------------------database-connect----------------------------------------------
+
+def connect():
+
+	connection=sqlite3.connect("CRUD")
+	slider=connection.cursor()
+	slider.execute(""" 
+
+	CREATE TABLE USERSDATA (
+
+		ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+		NAME VARCHAR(50),
+		PASSWORD VARCHAR(50),
+		SURNAME VARCHAR(10),
+		ADDRESS VARCHAR(50),
+		COMMENTS VARCHAR(100))
+
+		""")
+
+	connection.close()
+
+	messagebox.showinfo("CRUD", "The database was succesfully created")
+
+
 
 root=Tk()
 
@@ -15,7 +42,7 @@ root.config(menu=bar_menu, width=300, height=300)
 #--------------menu----------------------------------------------#
 
 menu_BBDD=Menu(bar_menu, tearoff=0)
-menu_BBDD.add_command(label="Connect")
+menu_BBDD.add_command(label="Connect", command=connect)
 menu_BBDD.add_command(label="Quit")
 bar_menu.add_cascade(label="BBDD", menu=menu_BBDD)
 
@@ -37,58 +64,58 @@ bar_menu.add_cascade(label="Help", menu=menu_help)
 
 #-------frame------------------------------------------------------#
 
-miframe=Frame(root, width=1200, height=600)
-miframe.pack()
-miframe.config()
+myframe=Frame(root, width=1200, height=600)
+myframe.pack()
+myframe.config()
 
 #--------entry-and-scrollbar------------------------------------------#
 
-id_text=Entry(miframe)
+id_text=Entry(myframe)
 id_text.grid(row=0, column=1)
 id_text.config(bg="#A9EBBB", justify="center")
 
-name_text=Entry(miframe)
+name_text=Entry(myframe)
 name_text.grid(row=1, column=1)
 name_text.config(bg="#A9EBBB", justify="center")
 
-password_text=Entry(miframe)
+password_text=Entry(myframe)
 password_text.grid(row=2, column=1)
 password_text.config(bg="#A9EBBB", justify="center", show="*")
 
-surname_text=Entry(miframe)
+surname_text=Entry(myframe)
 surname_text.grid(row=3, column=1)
 surname_text.config(bg="#A9EBBB", justify="center")
 
-address_text=Entry(miframe)
+address_text=Entry(myframe)
 address_text.grid(row=4, column=1)
 address_text.config(bg="#A9EBBB", justify="center")
 
-comments_text=Text(miframe, width=16, height=5)
+comments_text=Text(myframe, width=16, height=5)
 comments_text.grid(row=5, column=1)
 comments_text.config(bg="#A9EBBB")
 
-scrollvert=Scrollbar(miframe, command=comments_text.yview)
+scrollvert=Scrollbar(myframe, command=comments_text.yview)
 scrollvert.grid(row=5, column=2, sticky="nsew")
 comments_text.config(yscrollcommand=scrollvert.set)
 
 #----------label--------------------------------------------------------#
 
-id_label=Label(miframe, text="Id: ")
+id_label=Label(myframe, text="Id: ")
 id_label.grid(row=0, column=0, sticky="s", padx=30, pady=30)
 
-name_label=Label(miframe, text="Name: ")
+name_label=Label(myframe, text="Name: ")
 name_label.grid(row=1, column=0, sticky="s", padx=30 , pady=30)
 
-password_label=Label(miframe, text="Password: ")
+password_label=Label(myframe, text="Password: ")
 password_label.grid(row=2, column=0, sticky="s", padx=30 , pady=30)
 
-surname_label=Label(miframe, text="Surname: ")
+surname_label=Label(myframe, text="Surname: ")
 surname_label.grid(row=3, column=0, sticky="s", padx= 30 , pady=30)
 
-address_label=Label(miframe, text="Address: ")
+address_label=Label(myframe, text="Address: ")
 address_label.grid(row=4, column=0, sticky="s", padx=30 , pady=30)
 
-comments_label=Label(miframe, text="Comments: ")
+comments_label=Label(myframe, text="Comments: ")
 comments_label.grid(row=5, column=0, sticky="s", padx=30 , pady=30)
 
 #------------button------------------------------------------------------#
@@ -105,6 +132,8 @@ button_delete.pack(side="right", padx=15, pady=15)
 button_update=Button(root, text="Update")
 button_update.pack(side="right", padx=15, pady=15)
 
-#----end---------------------------------------------------------------#
+#----------------end---------------------------------------------------------------#
 
 root.mainloop()
+
+
