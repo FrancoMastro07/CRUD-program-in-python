61df3cc24146909ee8a74a8a4e389c10d150121b
FrancoMastro07 <francomastro07@gmail.com>
2023-02-24 11:46:19 -0300

    Added a timer

--- First_Project/Game.py
+++ First_Project/Game.py
@@ -21,12 +21,15 @@ cactus_x=700
 cactus_y=330
 
 player_speed=0
-cactus_speed=0
+cactus_speed=10
 
-#-----------logic-----------------------------#
+allow_input=True
+
+#-----------logic-----------------------------------------#
 
 player=pygame.image.load("bro.png").convert()
 cactus=pygame.image.load("cactus.png").convert()
+clouds=pygame.image.load("clouds.png").convert()
 cactus.set_colorkey(WHITE)
 
 for i in range(200):
@@ -47,24 +50,40 @@ while True:
 	
 			if event.key==pygame.K_SPACE:
 
-				player_speed-=7			
+				if allow_input:
+					
+					player_speed-=10
+					allow_input=False
+					pygame.time.set_timer(pygame.USEREVENT, 2000)
+
+		elif event.type==pygame.USEREVENT:
 		
+			allow_input=True													
+
 	player_y+=player_speed
 
-	if player_y==200:
+	if player_y<=160:
 
 		player_speed*=-1
 
 	elif player_y==298:
 
 		player_speed*=0
+			
+	cactus_x-=cactus_speed	
 
-#----------drawing-zone---------------------#
+	if cactus_x==-10:
+
+		cactus_x=800
+
+
+#----------drawing-zone-------------------------------------#
 
 	screen.fill(WHITE)	
 	screen.blit(player, [player_x, player_y])
 	screen.blit(cactus, [cactus_x, cactus_y])
-
+	screen.blit(clouds, [40, 10])
+	
 	pygame.draw.line(screen, BLACK, [800,400], [0,400], 5)
 	pygame.draw.circle(screen, BLACK, (600,90), 30)	
 
