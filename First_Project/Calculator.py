ba3bad1a02310ce39b7ab058d7ff74add024012d
FrancoMastro07 <111392780+FrancoMastro07@users.noreply.github.com>
2024-08-13 20:09:48 -0300

    Calculator reworked
    I decided to rework the calculator years later.

--- First_Project/Calculator.py
+++ First_Project/Calculator.py
@@ -1,282 +1,168 @@
-from tkinter import *
+import tkinter as tk
 
-#----------------root-------------------
-
-root=Tk()
+root = tk.Tk()
 root.title("Calculator")
 root.iconbitmap("Calculator.ico")
-
-
-#-------------frame--------------------------------------
-
-frame=Frame(root)
-frame.pack()
-
-#-------------entry---------------------------------------
-
-ScreenNumber=StringVar()
-entry_number=Entry(frame, width=30, textvariable=ScreenNumber)
-entry_number.grid(row=1, column=1, padx=10, pady=10, columnspan=6)
-entry_number.config(background="black", fg="#03f943", justify="right")
-
-#------------------------code-----------------------------------------
-
-reset_screen=False
-
-operation=""
-
-result=0
-
-def Clicked_Num(num):
-
-	global reset_screen
-
-	if reset_screen!=False:
-
-		ScreenNumber.set(num)
-
-		reset_screen=False
-
-	else:
-
-		return ScreenNumber.set(ScreenNumber.get()+num)
-
-def sum(num):
-
-	global operation
-
-	global result
-
-	global reset_screen
-
-	result+=int(num)
-
-	operation="suma"
-
-	reset_screen=True
-
-	ScreenNumber.set(result)
-
-num1=0
-
-clicker_rest=0
-
-def rest(num):
-
-	global operation
-
-	global result
-
-	global reset_screen
-
-	global num1
-
-	global clicker_rest
-
-	if clicker_rest==0:
-
-		num1=int(num)
-
-		result=num1
-
-	else:
-
-		if clicker_rest==1:
-
-			result=num1-int(num)
-
-		else:
-
-			result=int(result)-int(num)
-
-		ScreenNumber.set(result)
-		
-		result=ScreenNumber.get()
-
-	clicker_rest+=1
-
-	operation="rest"
-
-	reset_screen=True
-
-clicker_multi=0
-
-def multi(num):
-
-	global result
-
-	global operation
-
-	global reset_screen
-
-	global num1
-
-	global clicker_multi
-
-	if clicker_multi==0:
-
-		num1=int(num)
-
-		result=num1
-
-	else:
-	
-		if clicker_multi==1:
-
-			result=num1*int(num)
-
-		else:
-
-			result=int(result)*int(num)
-
-		ScreenNumber.set(result)
-
-		result=ScreenNumber.get()
-
-	clicker_multi+=1
-
-	operation="multi"
-
-	reset_screen=True	
-
-clicker_div=0
-
-def div(num):
-
-	global result
-
-	global operation
-
-	global reset_screen
-
-	global num1
-
-	global clicker_div
-
-	if clicker_div==0:
-
-		num1=float(num)
-
-		result=num1
-
-	else:
-
-		if clicker_div==1:
-
-			result=num1/float(num)
-
-		else:
-
-			result=float(result)/float(num)
-
-		ScreenNumber.set(result)
-		
-		result=ScreenNumber.get()
-
-	clicker_div+=1
-	
-	operation="div"
-
-	reset_screen=True
-
+root.geometry("464x400")
+root.configure(bg="#DADADA")
+root.resizable(False, False)
+
+number = tk.StringVar()
+number.initialize("")
+number_font = ("Arial", 27)
+
+entry = tk.Entry(root, textvariable=number, font=number_font, state="readonly")
+entry.grid(row=0, column=0, columnspan=5, padx=10, ipady=15, sticky="WE")
+
+counter=0
+
+while counter<5:
+    
+    root.grid_columnconfigure(counter, weight=1)
+    root.grid_rowconfigure(counter, weight=1)
+    counter+=1
+
+numbers_list=[]
+operation=0
+math_op=0
+op_counter=False
+
+#--------------------------defs-----------------------------------------------------------------------------
+
+def change(num):
+    
+    if number.get()=="" or number.get()=="Syntax error" or number.get()=="MATH error":
+        number.initialize(num)
+    else:
+        num_to_change=number.get()
+        num_changed=num_to_change+num
+        number.set(num_changed)
+        
+def operate(op):
+    
+    global operation
+    global math_op
+    global op_counter
+    
+    try:
+        op_counter=True
+        math_op=0
+        operation=op
+        first_num=number.get()
+        numbers_list.append(float(first_num))
+        number.initialize("")
+    except ValueError:
+        number.set("Syntax error")                         
+
+def equal():    
+                                         
+    global operation
+    global math_op
+    global op_counter
+    
+    if op_counter:
+        try:
+            last_num=number.get()
+            numbers_list.append(float(last_num))
+            math_op=numbers_list[0]
+            del numbers_list[0]
+            
+            if operation==1:
+                for i in numbers_list:
+                    math_op+=i
+            elif operation==2:
+                for i in numbers_list:
+                    math_op-=i
+            elif operation==3:
+                for i in numbers_list:
+                    math_op*=i
+            elif operation==4:
+                for i in numbers_list:
+                    math_op/=i
+                    
+            if math_op==-0:
+                math_op=0
+    
+            new_list=list(str(math_op))
+            list_rev=new_list[::-1]
+            new_list_2=[]
+            
+            for i in list_rev:
+                if i!=".":
+                    new_list_2.append(i)
+                else:
+                    break
+                
+            if new_list_2[0]=="0" and len(new_list_2)==1:
+                number.set(str(int(math_op)))
+            else:
+                number.set(str(round(float(math_op), 22)))   
+        except ZeroDivisionError:
+            number.set("MATH error")
+    numbers_list.clear()
+    operation=0
+    
 def delete():
+    
+    global operation
+    global math_op
+    
+    number.initialize("")
+    numbers_list.clear()
+    math_op=0
+    operation=0
+            
+#------------------------buttons-------------------------------------------------
 
-	global result
+button_0 = tk.Button(root, text="0", command=lambda: change("0"))
+button_1 = tk.Button(root, text="1", command=lambda: change("1"))
+button_2 = tk.Button(root, text="2", command=lambda: change("2"))
+button_3 = tk.Button(root, text="3", command=lambda: change("3"))
+button_4 = tk.Button(root, text="4", command=lambda: change("4"))
+button_5 = tk.Button(root, text="5", command=lambda: change("5"))
+button_6 = tk.Button(root, text="6", command=lambda: change("6"))
+button_7 = tk.Button(root, text="7", command=lambda: change("7"))
+button_8 = tk.Button(root, text="8", command=lambda: change("8"))
+button_9 = tk.Button(root, text="9", command=lambda: change("9"))
+button_subt_symbol = tk.Button(root, text="-", command=lambda: change("-"))
+button_coma = tk.Button(root, text=".", command=lambda: change("."))
 
-	global reset_screen
+button_plus = tk.Button(root, text="+", command=lambda: operate(1))
+button_subt = tk.Button(root, text="-", command=lambda: operate(2))
+button_multi = tk.Button(root, text="*", command=lambda: operate(3))
+button_div = tk.Button(root, text="/", command=lambda: operate(4))
+button_equal = tk.Button(root, text="=", command=equal)
+button_delete = tk.Button(root, text="DEL", command=delete)
 
-	ScreenNumber.set("")
+button_dis_1 = tk.Button(root, text="")
+button_dis_1.config(state="disabled")
+button_dis_2 = tk.Button(root, text="")
+button_dis_2.config(state="disabled")
 
-	result=0		
+#------------------------------------button-grid-------------------------------------------------
 
-def results():
+button_7.grid(row=1, column=0, padx=5, pady=5, sticky="NSEW")
+button_8.grid(row=1, column=1, padx=5, pady=5, sticky="NSEW")
+button_9.grid(row=1, column=2, padx=5, pady=5, sticky="NSEW")
+button_delete.grid(row=1, column=3, padx=5, pady=5, sticky="NSEW")
+button_dis_1.grid(row=1, column=4, padx=5, pady=5, sticky="NSEW")
 
-	global result
+button_4.grid(row=2, column=0, padx=5, pady=5, sticky="NSEW")
+button_5.grid(row=2, column=1, padx=5, pady=5, sticky="NSEW")
+button_6.grid(row=2, column=2, padx=5, pady=5, sticky="NSEW")
+button_multi.grid(row=2, column=3, padx=5, pady=5, sticky="NSEW")
+button_div.grid(row=2, column=4, padx=5, pady=5, sticky="NSEW")
 
-	global operation
+button_1.grid(row=3, column=0, padx=5, pady=5, sticky="NSEW")
+button_2.grid(row=3, column=1, padx=5, pady=5, sticky="NSEW")
+button_3.grid(row=3, column=2, padx=5, pady=5, sticky="NSEW")
+button_plus.grid(row=3, column=3, padx=5, pady=5, sticky="NSEW")
+button_subt.grid(row=3, column=4, padx=5, pady=5, sticky="NSEW")
 
-	if "suma" in operation:
-
-		ScreenNumber.set(int(result)+int(ScreenNumber.get()))
-
-		result=0
-
-	elif "rest" in operation:
-
-		ScreenNumber.set(int(result)-int(ScreenNumber.get()))
-
-		result=0
-
-		clicker_rest=0
-
-	elif "multi" in operation:
-
-		ScreenNumber.set(int(result)*int(ScreenNumber.get()))
-
-		result=0
-
-		clicker_multi=0
-
-	elif "div" in operation:
-
-		ScreenNumber.set(int(result)/int(ScreenNumber.get()))
-
-		result=0
-
-		clicker_div=0
-
-
-	
-#----------------buttons--------------------------------------------------------
-
-button_7=Button(frame, text="7", width=3, command=lambda:Clicked_Num("7"))
-button_7.grid(row=2, column=1, padx=10, pady=10)
-
-button_8=Button(frame, text="8", width=3, command=lambda:Clicked_Num("8"))
-button_8.grid(row=2, column=2, padx=10, pady=10)
-
-button_9=Button(frame, text="9", width=3, command=lambda:Clicked_Num("9"))
-button_9.grid(row=2, column=3, padx=10, pady=10)
-
-button_4=Button(frame, text="4", width=3, command=lambda:Clicked_Num("4"))
-button_4.grid(row=3, column=1, padx=10, pady=10)
-
-button_5=Button(frame, text="5", width=3, command=lambda:Clicked_Num("5"))
-button_5.grid(row=3, column=2, padx=10, pady=10)
-
-button_6=Button(frame, text="6", width=3, command=lambda:Clicked_Num("6"))
-button_6.grid(row=3, column=3, padx=10, pady=10)
-
-button_1=Button(frame, text="1", width=3, command=lambda:Clicked_Num("1"))
-button_1.grid(row=4, column=1, padx=10, pady=10)
-
-button_2=Button(frame, text="2", width=3, command=lambda:Clicked_Num("2"))
-button_2.grid(row=4, column=2, padx=10, pady=10)
-
-button_3=Button(frame, text="3", width=3, command=lambda:Clicked_Num("3"))
-button_3.grid(row=4, column=3, padx=10, pady=10)
-
-button_0=Button(frame, text="0", width=3, command=lambda:Clicked_Num("0"))
-button_0.grid(row=5, column=2, padx=10, pady=10)
-
-button_sum=Button(frame, text="+", width=3, command=lambda:sum(ScreenNumber.get()))
-button_sum.grid(row=2, column=4, padx=10, pady=10)
-
-button_x=Button(frame, text="x", width=3, command=lambda:multi(ScreenNumber.get()))
-button_x.grid(row=3, column=4, padx=10, pady=10)
-
-button_delete=Button(frame, text="Del", width=3, command=lambda:delete())
-button_delete.grid(row=4, column=4, padx=10, pady=10)
-
-button_rest=Button(frame, text="-", width=3, command=lambda:rest(ScreenNumber.get()))
-button_rest.grid(row=2, column=5, padx=10, pady=10)
-
-button_div=Button(frame, text="/", width=3, command=lambda:div(ScreenNumber.get()))
-button_div.grid(row=3, column=5, padx=10, pady=10)
-
-button_equal=Button(frame, text="=", width=3, command=lambda:results())
-button_equal.grid(row=4, column=5, padx=10, pady=10)
-
-#--------------------------end---------------------------------------------
+button_coma.grid(row=4, column=0, padx=5, pady=5, sticky="NSEW")
+button_0.grid(row=4, column=1, padx=5, pady=5, sticky="NSEW")
+button_subt_symbol.grid(row=4, column=2, padx=5, pady=5, sticky="NSEW")
+button_equal.grid(row=4, column=3, padx=5, pady=5, sticky="NSEW")
+button_dis_2.grid(row=4, column=4, padx=5, pady=5, sticky="NSEW")
 
 root.mainloop()
